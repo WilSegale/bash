a685fa9f73ffb416096ef25d8988a1e01e321228
Will Segale <39059459+WilSegale@users.noreply.github.com>
2022-12-12 09:52:04 -0500

    scripts

--- removeExtensions.sh
+++ removeExtensions.sh
@@ -1,3 +1,9 @@
 cd ~/.vscode/extensions
-rm -rf *
-echo "All extensions from vscode have been removed. You will have to reset vscode"
+echo "Are you sure that you want to remove all extensions"
+read -p "YES OR NO: " YesOrNo
+if [[ $YesOrNo == "YES" ]] then
+    rm -rf *
+    echo "All extensions from vscode have been removed. You will have to reset vscode"
+else
+    exit
+fi
