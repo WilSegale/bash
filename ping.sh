10157752e527b68671fcdc7d7a2521a528b7f5e8
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-02-16 07:20:12 -0500

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -2,10 +2,10 @@ read -p "input a site to ping: " site
 while True
 do
   if ping -q -c 2 -W 1 $site > /dev/null; then
-    echo "https://$site is up"
+    echo "https://$site is up 😀"
 	  break;
   else
-	  echo "https://$site is down"
+	  echo "https://$site is down 😭"
   fi
 sleep 2 
 done
