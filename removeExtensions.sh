8ece4869b323c6872b9fd68217ef7312a7314317
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-11 17:42:47 -0400

    Update removeExtensions.sh

--- removeExtensions.sh
+++ removeExtensions.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-if which code >/dev/null; then
-    echo "Visual Studio Code is installed"
-    cd ~/.vscode/extensions
-    echo "Are you sure that you want to remove all extensions"
-    read -p "YES OR NO: " YesOrNo
+cd ~/.vscode/extensions
+echo "Are you sure that you want to remove all extensions"
+read -p "YES OR NO: " YesOrNo
 
-    if  [[ $YesOrNo == "YES" ]] || 
-        [[ $YesOrNo == "yes" ]] || 
-        [[ $YesOrNo == "y" ]] || 
-        [[ $YesOrNo == "Y" ]]; then
-        rm -rf *
-        echo "All extensions from vscode have been removed. You will have to reset vscode"
-    else
-        echo "Ok your extensions will not be removed"
-        exit
-    fi
+if  [[ $YesOrNo == "YES" ]] || 
+    [[ $YesOrNo == "yes" ]] || 
+    [[ $YesOrNo == "y" ]] || 
+    [[ $YesOrNo == "Y" ]]; then
+    rm -rf *
+    echo "All extensions from vscode have been removed. You will have to reset vscode"
 else
-    echo "Visual Studio Code is not installed"
+    echo "Ok your extensions will not be removed"
+    exit
 fi
+
