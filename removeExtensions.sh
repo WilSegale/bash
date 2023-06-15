ebb6f196a0c2bd79ce996c14fb039ffd82131d02
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-06-15 12:54:35 -0400

    Update removeExtensions.sh

--- removeExtensions.sh
+++ removeExtensions.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+yes=("yes" "Yes" "YES" "y" "Y")
 cd ~/.vscode/extensions
 echo "Are you sure that you want to remove all extensions"
 read -p "YES OR NO: " YesOrNo
 
-if  [[ $YesOrNo == "YES" ]] || 
-    [[ $YesOrNo == "yes" ]] || 
-    [[ $YesOrNo == "y" ]] || 
-    [[ $YesOrNo == "Y" ]]; then
+if [[ " ${yes[*]} " == *" $YesOrNo "* ]]; then
     rm -rf *
     echo "All extensions from vscode have been removed. You will have to reset vscode."
 else
