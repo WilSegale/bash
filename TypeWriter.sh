4fa6df4af268be12d1d40e8df76cb0c21c924870
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-06-24 09:00:20 -0400

    bash

---
+++ TypeWriter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+typewriter_effect() {
+  text=$1
+  delay=$2
+
+  for (( i=0; i<${#text}; i++ )); do
+    printf "%c" "${text:$i:1}"
+    sleep "$delay"
+  done
+
+  echo
+}
+
+# Example usage
+typewriter_effect "Hello, World!" 0.1
+typewriter_effect "This is a typewriter effect in Bash." 0.05
+
