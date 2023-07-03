d93356d443d666101f95832604e00cdba3d317e1
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-03 11:27:53 -0400

    Update removeExtensions.sh

--- removeExtensions.sh
+++ removeExtensions.sh
@@ -5,7 +5,7 @@ RED='\033[31m'
 RESET='\033[0m'
 yes=("yes" "Yes" "YES" "y" "Y")
 
-extensions_dir="~/.vscode/extensions"
+extensions_dir="${HOME}/.vscode/extensions"
 
 if ! cd "$extensions_dir"; then
     echo -e "${RED}[-]${RESET} ERROR"
@@ -15,22 +15,27 @@ fi
 
 echo "Are you sure you want to remove all extensions?"
 read -p "YES OR NO: " YesOrNo
-
 if [[ " ${yes[*]} " == *" $YesOrNo "* ]]; then
-    for file in *; do
-        if [ -d "$file" ]; then
-            echo "Deleting extension: $file"
-            rm -rf "$file"
-        fi
-    done
 
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}[+]${RESET} DONE"
-        echo -e "${GREEN}[+]${RESET} All extensions from vscode have been removed. You will have to restart vscode."
-    else
-        echo -e "${RED}[-]${RESET} ERROR"
-        echo -e "${RED}[-]${RESET} Failed to remove extensions."
+    if [[ -z "$(find "${extensions_dir}" -mindepth 1 -print -quit)" ]]; then
+        echo "Folder is empty"
+    else 
+        for file in *; do
+            if [ -d "$file" ]; then
+                echo "Deleting extension: $file"
+                rm -rf "$file"
+            fi
+        done
+
+        if [ $? -eq 0 ]; then
+            echo -e "${GREEN}[+]${RESET} DONE"
+            echo -e "${GREEN}[+]${RESET} All extensions from vscode have been removed. You will have to restart vscode."
+        else
+            echo -e "${RED}[-]${RESET} ERROR"
+            echo -e "${RED}[-]${RESET} Failed to remove extensions."
+        fi
     fi
+
 else
     echo -e "${RED}[-]${RESET} DONE"
     echo -e "${RED}[-]${RESET} Ok, your extensions will not be removed."
