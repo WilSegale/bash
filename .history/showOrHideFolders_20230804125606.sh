05c2ed4faf9b8c68c828b853369017b7b906ba42
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-04 12:58:39 -0400

    bash

---
+++ .history/showOrHideFolders_20230804125606.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+read -p "Do you want to show or hide folders: " ShowOrHide
+
+show=("Show" "show" "SHOW")
+hide=("Hide" "hide" "HIDE")
+
+show_hide(){
+    local found=false
+    for option in "${show[@]}"; do
+        if [[ "$option" == "$ShowOrHide" ]]; then
+            defaults write com.apple.finder AppleShowAllFiles -bool true
+            killall Finder
+            found=true
+            break
+        fi
+    done
+
+    if [[ "$found" == false ]]; then
+        for option in "${hide[@]}"; do
+            if [[ "$option" == "$ShowOrHide" ]]; then
+                defaults write com.apple.finder AppleShowAllFiles -bool false
+                killall Finder
+                found=true
+                break
+            fi
+        done
+    fi
+
+    if [[ "$found" == false ]]; then
+        echo "Unknown option"
+
+      show_hide
+    fi
+}
+
+show_hide
---
+++ .history/showOrHideFolders_20230804125759.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+read -p "Do you want to show or hide folders: " ShowOrHide
+
+show=("Show" "show" "SHOW")
+hide=("Hide" "hide" "HIDE")
+
+function show_hide(){
+    local found=false
+    for option in "${show[@]}"; do
+        if [[ "$option" == "$ShowOrHide" ]]; then
+            defaults write com.apple.finder AppleShowAllFiles -bool true
+            killall Finder
+            found=true
+            break
+        fi
+    done
+
+    if [[ "$found" == false ]]; then
+        for option in "${hide[@]}"; do
+            if [[ "$option" == "$ShowOrHide" ]]; then
+               defaults write com.apple.finder AppleShowAllFiles -bool false
+               killall Finder
+                found=true
+                break
+            fi
+        done
+    fi
+
+    if [[ "$found" == false ]]; then
+        echo "Unkown option"
+        show_hide
+    fi
+}
+
+show_hide
