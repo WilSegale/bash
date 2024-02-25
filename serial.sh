5d18078cfd53192ef2c907187fdaf94bfe205216
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-02-24 20:55:00 -0500

    serail

--- serial.sh
+++ serial.sh
@@ -1,5 +1,11 @@
-cd /dev
-sleep 1 
-ls tty.*
-read -p "Input the serail device you want to connect to: " serailDeviceName
-screen "${serailDeviceName}"
+cd /dev devices
+command $devices
+if [ $? -ne 0 ]; then
+    echo "ERROR:Nothing on the screen to display"
+else
+    echo "These are the serail devices on your computer."
+    sleep 1 
+    ls tty.*
+    read -e -p "Input the serail device you want to connect to: " serailDeviceName
+    screen "${serailDeviceName}"
+fi
